31b4155d3329435f75e558092e0aeed6fd31fce8
Jaideepk2004 <147708173+Jaideepk2004@users.noreply.github.com>
2026-01-28 14:51:05 +0530

    27-01-26

---
+++ 27-01-26/problem1.java
@@ -0,0 +1,61 @@
+import java.io.*;
+
+class NoMatter extends Exception {
+    public NoMatter(String msg) {
+        super(msg);
+    }
+}
+
+public class problem1 {
+    static void EmptyFile(int str) throws NoMatter{
+        if(str == -1){
+            throw new NoMatter("the file is empty");
+        }
+    }
+    public static void main(String[] args){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("problem1.txt"));
+            BufferedWriter bw  = new BufferedWriter(new FileWriter("ReverseProblem1.txt"));
+            String line;
+            int Noline = 0;
+            int NoWords = 0;
+            int NoCharaters = 0;
+            EmptyFile(br.read());
+            while ((line = br.readLine()) != null)
+            {
+                Noline++;
+                NoCharaters += line.length();
+                for (int i = 0; i < line.length(); i++) {
+                    if (line.charAt(i) == ' ') {
+                        NoWords++;
+                    }
+                }
+
+                String[] word = line.split(" ");
+                for (int j = word.length - 1; j >= 0; j--) {
+                    bw.write(word[j]);
+                    bw.write(' ');
+                }
+                bw.newLine();
+            }
+
+            System.out.println("Noline :- " + Noline);
+            System.out.println("NoWords :- " + (NoWords + Noline)); 
+            System.out.println("NoCharaters :- " + NoCharaters);
+            br.close();
+            bw.close();
+
+        } 
+        catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } 
+        catch (IOException e) {
+            System.out.println("IO Error");
+        }
+        catch(NoMatter e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
+
